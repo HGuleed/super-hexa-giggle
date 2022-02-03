11dc0d354822bb9e2607e8f6048def14efb68dac
Hodan <you@example.com>
2022-02-03 07:53:14 -0500

    created models for users and articles, and hashed the password

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require("./User");
+const Article = require("./Article");
+
+User.hasMany(Article, {
+  foreignKey: "article_id",
+  onDelete: "SET NULL",
+});
+
+Article.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
